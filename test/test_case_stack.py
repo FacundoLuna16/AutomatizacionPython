2a0f34399bc4f981510e48feb0fd229fb4336698
Facundo Luna <89422@sistemas.frc.utn.edu.ar>
2024-01-30 16:16:28 -0300

    agregamos step en test para salidas de la documentacion

--- test/test_case_stack.py
+++ test/test_case_stack.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from pages.stack_home_page import StackHomePage
 from pages.stack_result_page import StackResultPage
 
@@ -9,21 +10,31 @@ parametros_busqueda = [
 class Tests:
 
     @pytest.mark.parametrize("var_buscar, resultado", parametros_busqueda)
+    @allure.title("Validar Busqueda desde la caja de texto")
+    @allure.description("Validar que la caja de texto funcione correctamente realizando una busqueda y validando el resultado")
     def test_validar_caja_texto_nuevo(self,var_buscar, resultado,driver):#driver viene de conftest.py los demas parametros vienen del parametrize
         """
         Test para validar la funcionalidad de la caja de búsqueda.
         """
-        driver.get("https://es.stackoverflow.com/")
-        print("Validamos que se cargue la página")
-        home_page = StackHomePage(driver)
-        home_page.click_aceptar_cookies()
-        home_page.buscar(var_buscar)
+        with allure.step("Nos dirigimos a la pagina Stack Overflow en español"):
+            print("Abrimos la página de Stack Overflow en español")
+            driver.get("https://es.stackoverflow.com/")
 
-        result_page = StackResultPage(driver)
-        result_page.validar_contenido_URL(resultado)
+        with allure.step("Validamos que se cargue la página"):         
+            home_page = StackHomePage(driver)
+        
+        with allure.step("Aceptamos las cookies"):
+            home_page.click_aceptar_cookies()
+        with allure.step("realizamos la busqueda"):
+            home_page.buscar(var_buscar)
+
+        with allure.step("Validamos la busqueda"):
+            result_page = StackResultPage(driver)
+            result_page.validar_contenido_URL(resultado)
 
 
-
+    @allure.title("Validar redireccion a la página de usuarios")
+    @allure.description("Validar que se redireccione a la página de usuarios")
     def test_ir_usuarios(self,driver):
         """
         Test para verificar la navegación a la página de usuarios.
